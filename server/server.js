fde3dcd6c7c5f069d1c26c22f0f03cc39afb002f
Rajesh Peketi <122203953+Rajesh0078@users.noreply.github.com>
2026-01-04 18:48:39 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { PORT, NODE_ENV } from "./config/env.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import connectToDb from "./config/database.js";
+import router from "./routes.js";
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send(`Caremind server is running on ${PORT} in ${NODE_ENV} env`);
+});
+
+app.use('/api/v1', router);
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`Caremind server is running on ${PORT} in ${NODE_ENV} env`);
+  connectToDb();
+});
